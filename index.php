ab877c0a32cdc0c9779956f58a43e941aac59916
Yasmine <yasmine.amaddah@laplateforme.io>
2023-02-09 10:10:04 +0100

    Initial commit

---
+++ index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css" />
+    <title>Reservation Salles</title>
+</head>
+
+<body class="body1">
+
+    <header>
+        <div class="head">
+            <nav>
+                <ul class="menu">
+                    <li><a class="titremenu" href="index.php">Home</a></li>
+                    <li><a class="titremenu" href="inscription.php">Inscription</a></li>
+                    <li><a class="titremenu" href="connexion.php">Connexion</a></li>
+                    <!-- <?php // if (isset($_SESSION['login']) == TRUE) {?> 
+                    <li><a class="titremenu" href="profil.php">Profil</a></li>
+                    <?php //} ?> -->
+                    <li><a class="titremenu" href=".php">Reservation</a>
+                        <ul>
+                            <li><a class="titremenu" href=".php">Planning</a></li>
+                            <li><a class="titremenu" href=".php">Formulaire de réservation</a></li>
+                        </ul>
+                    </li>
+                </ul>
+            </nav>
+        </div>
+    </header>
+
+    <!-- Body -->
+
+
+</body>
+
+</html>
+
+</html>
