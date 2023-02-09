a5a860b6b7508c4f5dec5d9a151b54bfdc12c87f
camille-clappaz <camille.clappaz@laplateforme.io>
2023-02-09 11:39:26 +0100

    index en cours

---
+++ footer-include.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <footer>
+        <h2>UNE AUTRE QUESTION</h2>
+        <p>Nous sommes joignables: <br>
+        - Par email contact@cinema.fr <br>
+        - Par telephone du lundi au vendredi de 8h à 23h <br>
+        - Sur les réseaux sociaux. <br>
+        &copy; All rights reserved <br>
+        <a href="https://github.com/camille-clappaz/reservation-salles">Repository GitHub</a>
+        </p>
+        
+    </footer>
+</body>
+</html>
